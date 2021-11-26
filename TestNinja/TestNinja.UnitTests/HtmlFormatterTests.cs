f002745f57ee784b2ce653e7713b0f6d18072cf0
Jean Paul Roux <rouxj2@LEGAL.REGN.NET>
2021-11-26 14:26:59 +0200

    1. String testing

---
+++ TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        private HtmlFormatter _htmlFormatter;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _htmlFormatter = new HtmlFormatter();
+        }
+
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            // Act
+            var result = _htmlFormatter.FormatAsBold("abc");
+
+            // Assert
+            // Remember string tests are case senstive in nUnit
+            // Very Specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            // More general
+            Assert.That(result, Does.StartWith("<strong>"));
+
+            // Possible alternative
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+        }
+    }
+}
