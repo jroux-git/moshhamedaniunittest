9e8f3f8ea134768a163124de16c62bbf983d5361
Jean Paul Roux <rouxj2@LEGAL.REGN.NET>
2021-11-26 15:01:27 +0200

    1. Testing void methods

---
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTests
+    {
+        private ErrorLogger _logger { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            // Act
+            _logger.Log("This is an error");
+
+            // Assert
+            Assert.That(_logger.LastError, Is.EqualTo("This is an error"));
+        }
+    }
+}
