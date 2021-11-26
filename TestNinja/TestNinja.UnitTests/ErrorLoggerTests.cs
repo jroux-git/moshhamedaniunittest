8853084b6fb348fccd09cddff0fc46412654dc71
Jean Paul Roux <rouxj2@LEGAL.REGN.NET>
2021-11-26 15:05:49 +0200

    1. Testing exception handling

--- TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -24,5 +24,16 @@ namespace TestNinja.UnitTests
             // Assert
             Assert.That(_logger.LastError, Is.EqualTo("This is an error"));
         }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ShouldThroughArgumentNullException(string error)
+        {
+            // Assert
+            // Have to create a delegate to test exceptions
+            Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+        }
     }
 }
