16f6b2665f91c71023d5617d2ff5e1e285115529
Jean Paul Roux <rouxj2@LEGAL.REGN.NET>
2021-11-26 15:12:17 +0200

    1. Testing raised events

--- TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -35,5 +35,22 @@ namespace TestNinja.UnitTests
             // Have to create a delegate to test exceptions
             Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
         }
+
+        [Test]
+        public void Log_ValidError_RaisedErrorLoggedEvent()
+        {
+            // Arrange
+            var id = Guid.Empty;
+            _logger.ErrorLogged += (sender, args) => { id = args; };
+
+            // Act
+            _logger.Log("This is an error");
+
+            // Assert
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+            
+
+
+        }
     }
 }
